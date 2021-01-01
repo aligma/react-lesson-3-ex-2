d90dd5f02e393218bda90d91fa757028c5d13609
David Huby <aligma@gmail.com>
2021-01-01 10:58:53 +0000

    Initial commit

---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+  render(
+    <h2>Forrest Gump</h2>
+    <p>Liked By:</p>
+    <ul>
+      <li>Nicholas Lain</li>
+    </ul>
+  );
+}
+
+export default Movie;
